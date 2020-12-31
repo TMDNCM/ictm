52a67bbf7c317a111678871d506900a9b8ca2827
m68kadse <13818469+m68kadse@users.noreply.github.com>
2020-12-31 05:53:18 +0100

    template/renderer: implement template loading

--- template/renderer.go
+++ template/renderer.go
@@ -1,13 +1,51 @@
 package template
 
-import(
-	"net/http"
-	"html/template"
+import (
+	//"net/http"
 	"github.com/Fliegermarzipan/gallipot/data"
+	"html/template"
+	"strings"
 )
 
-type FrontendData struct{
-	request *http.Request
-	loggedIn bool
-	user *data.User
+type FrontendData struct {
+	//Request *http.Request
+	LoggedIn bool
+	User     *data.User
+}
+
+var (
+	indexTemplate *template.Template
+)
+
+func LoadTemplates() {
+	funcMap := template.FuncMap{
+		"Split": strings.Split,
+		"Contains": func(s []string, e string) bool {
+			for _, a := range s {
+				if a == e {
+					return true
+				}
+			}
+			return false
+		},
+		"Combine": func(s ...[]string) []string {
+			ret := []string{}
+			for i := range s {
+				ret = append(ret, s[i])
+			}
+			return ret
+		},
+	}
+
+	//indexTemplate = template.Must(template.ParseFiles("index.html", "sidebar.html",
+	//	"head.html", "login.html")).Funcs(funcMap)
+
+	indexTemplate = template.Must(template.ParseGlob("*.html")).Funcs(funcMap).Lookup("index.html")
+}
+
+func GetTemplates() *template.Template {
+	if indexTemplate == nil {
+		LoadTemplates()
+	}
+	return indexTemplate
 }
