18540bfe85497cd7cb7f6a2abfd246ad083f7d9c
Robin Broda <robin@broda.me>
2021-01-02 19:39:58 +0100

    template/(renderer,index,head): unify include logic; try loading additional css if present

--- template/renderer.go
+++ template/renderer.go
@@ -4,30 +4,23 @@ import (
 	//"net/http"
 	"github.com/Fliegermarzipan/gallipot/data"
 	"html/template"
-	"strings"
 	"log"
+	"os"
 	"path"
+	"path/filepath"
+	"strings"
 )
 
 type FrontendData struct {
 	//Request *http.Request
 	LoggedIn bool
-	Page string
+	Page     string
 	User     *data.User
 }
 
 var (
 	indexTemplate *template.Template
 )
-
-func templateFromFile(funcMap template.FuncMap, s string, d interface{}) string {
-		var buf strings.Builder
-		err := template.Must(template.New(path.Base(s)).Funcs(funcMap).ParseFiles(s)).Execute(&buf, d)
-		if err != nil {
-			log.Print(err)
-		}
-		return buf.String()
-}
 
 func LoadTemplates() {
 	funcMap := template.FuncMap{
@@ -50,19 +43,35 @@ func LoadTemplates() {
 		"list": func(s ...interface{}) []interface{} {
 			return s
 		},
-		"title": func(s string) string {
-			return strings.Title(strings.ToLower(s))
+		"lower": strings.ToLower,
+		"title": strings.Title,
+		"exists": func(s string) bool {
+			if _, err := os.Stat("/path/to/whatever"); err == nil {
+				return true
+			} else if os.IsNotExist(err) {
+				return false
+			}
+			log.Print("file exists fucked up")
+			return false
 		},
 	}
-	funcMap["include"] = func(s string, d interface{}) template.HTML {
-		return template.HTML(templateFromFile(funcMap, s, d))
-	}
-	funcMap["includeCSS"] = func(s string, d interface{}) template.CSS {
-		return template.CSS(templateFromFile(funcMap, s, d))
-	}
+	funcMap["include"] = func(s string, d interface{}) interface{} {
+		ext := filepath.Ext(s)
 
-	//indexTemplate = template.Must(template.ParseFiles("index.html", "sidebar.html",
-	//	"head.html", "login.html")).Funcs(funcMap)
+		var buf strings.Builder
+		err := template.Must(template.New(path.Base(s)).Funcs(funcMap).ParseFiles(s)).Execute(&buf, d)
+		if err != nil {
+			log.Print(err)
+		}
+		rendered := buf.String()
+
+		if ext == ".html" {
+			return template.HTML(rendered)
+		} else if ext == ".css" {
+			return template.CSS(rendered)
+		}
+		return rendered
+	}
 
 	indexTemplate = template.Must(template.New("").Funcs(funcMap).ParseGlob("*.html")).Lookup("index.html")
 }
